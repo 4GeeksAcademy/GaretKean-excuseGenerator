0dc4746bcf9e31af278b21a318c3884accae1e33
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-06-29 18:06:04 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,40 +2,74 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
+// let who = ["The dog", "My grandma", "The mailman", "My bird"];
+// let action = ["ate", "peed", "crushed", "broke"];
+// let what = ["my homework", "my phone", "the car"];
+// let when = [
+// "before the class",
+// "when I was sleeping",
+// "while I was exercising",
+//"during my lunch",
+// "while I was praying"
+// ];
 
-function getRandomNumber(a, b) {
-  return Math.floor(Math.random() * (b - a) + a);
-}
+// function getRandomNumber(a, b) {
+//   return Math.floor(Math.random() * (b - a) + a);
+// }
 
-function getRandom(anyArray) {
-  let max = anyArray.length - 1;
-  let min = 0;
-  let random = getRandomNumber(min, max);
-  return anyArray[random];
-}
+// function getRandom(anyArray) {
+//   let max = anyArray.length - 1;
+//   let min = 0;
+//   let random = getRandomNumber(min, max);
+//   return anyArray[random];
+// }
+
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+//   document.querySelector("#excuse").innerHTML =
+//     getRandom(who) +
+//     " " +
+//     getRandom(action) +
+//     " " +
+//     getRandom(what) +
+//     " " +
+//     getRandom(when);
+// };
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  document.querySelector("#excuse").innerHTML =
-    getRandom(who) +
+  console.log("Hello World");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
     " " +
-    getRandom(action) +
+    action[actionIndex] +
     " " +
-    getRandom(what) +
+    what[whatIndex] +
     " " +
-    getRandom(when);
+    when[whenIndex]
+  );
 };
